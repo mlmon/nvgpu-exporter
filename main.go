26ec899218aeedcbdd56eaacc032bae03487bb74
Nathan Fisher <nfisher@junctionbox.ca>
2025-10-23 11:12:04 -0400

    Add pciBusIdToString for converting BusIdLegacy field

--- main.go
+++ main.go
@@ -78,7 +78,7 @@ func getGpuVersionDetail(device nvml.Device) (*GpuInfo, error) {
 	if !errors.Is(ret, nvml.SUCCESS) {
 		return nil, fmt.Errorf("failed to get PCI info: %v", nvml.ErrorString(ret))
 	}
-	info.PciBusId = string(pciInfo.BusIdLegacy[:])
+	info.PciBusId = pciBusIdToString(pciInfo.BusIdLegacy)
 
 	// Get name
 	name, ret := device.GetName()
@@ -249,7 +249,7 @@ func collectFabricHealth(devices []nvml.Device) {
 			log.Printf("Failed to get PCI info for device %s: %v", uuid, nvml.ErrorString(ret))
 			continue
 		}
-		pciBusId := string(pciInfo.BusIdLegacy[:])
+		pciBusId := pciBusIdToString(pciInfo.BusIdLegacy)
 
 		// Get GPU fabric info - try V2 which includes health mask
 		fabricInfo, ret := device.GetGpuFabricInfoV().V2()
@@ -278,6 +278,22 @@ func collectFabricHealth(devices []nvml.Device) {
 		accessTimeoutRecovery := (fabricInfo.HealthMask >> 6) & 0x3
 		fabricHealth.WithLabelValues(uuid, pciBusId, "access_timeout_recovery").Set(flagToGauge(accessTimeoutRecovery != 1))
 	}
+}
+
+// pciBusIdToString converts a PCI bus ID byte array to a human-readable string
+// Standard PCI address format is: DDDD:BB:DD.F (e.g., 0000:00:1e.0)
+// This is typically 12-13 characters long
+func pciBusIdToString(busId [16]uint8) string {
+	// Standard PCI address is domain:bus:device.function (12-13 chars)
+	// Find the end by looking for common PCI address length
+	str := string(busId[:])
+	// Find the last digit or period in the expected PCI format
+	for i := 12; i < len(busId) && i < 14; i++ {
+		if busId[i] == 0 || busId[i] < 32 || busId[i] > 126 {
+			return str[:i]
+		}
+	}
+	return str[:13]
 }
 
 // flagToGauge converts a boolean to a float64 for Prometheus gauges
